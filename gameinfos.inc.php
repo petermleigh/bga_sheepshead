9e89e80ec107327fd9107f1d5dd4e3f117614696
Peter Leigh <petermleigh@gmail.com>
2024-01-07 13:31:52 -0700

    ignored vscode folder

--- gameinfos.inc.php
+++ gameinfos.inc.php
@@ -13,7 +13,7 @@
 $gameinfos = array( 
 
 // Name of the game in English (will serve as the basis for translation) 
-'game_name' => "My Great Game",
+'game_name' => "Sheepshead",
 
 // Game designer (or game designers, separated by commas)
 'designer' => 'John Doe',       
@@ -38,11 +38,11 @@ $gameinfos = array(
 
 
 // Players configuration that can be played (ex: 2 to 4 players)
-'players' => array( 2,3,4 ),    
+'players' => array( 5 ),    
 
 // Suggest players to play with this number of players. Must be null if there is no such advice, or if there is only one possible player configuration.
 // NB: the automatic lobby will try first the lowest number of players if this is not specified. So you _have to_ specify this parameter if the lowest player number is not compatible with the default options.
-'suggest_player_number' => null,
+'suggest_player_number' => 5,
 
 // Discourage players to play with these numbers of players. Must be null if there is no such advice.
 'not_recommend_player_number' => null,
